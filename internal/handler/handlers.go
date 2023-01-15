ff5d97af5d8db5d850aa90a7391435487af92fa3
454270186 <sc21ey@leeds.ac.uk>
2023-01-15 09:12:52 +0800

    fix some errors

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -34,7 +34,7 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
-	render.RenderTemplate(w, r, "home.page.html", &Models.TemplateData{})
+	_ = render.RenderTemplate(w, r, "home.page.html", &Models.TemplateData{})
 }
 
 // About is the about page handler
@@ -47,7 +47,7 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap["remote_ip"] = remoteIP
 
 	// render the template
-	render.RenderTemplate(w, r, "about.page.html", &Models.TemplateData{
+	_ = render.RenderTemplate(w, r, "about.page.html", &Models.TemplateData{
 		StringMap: stringMap,
 	})
 }
@@ -59,7 +59,7 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 	data["reservation"] = emptyReservation
 	// initialize empty data and form-data to make-reservation page
 	// so that it can display blank in every input when first time get in this page
-	render.RenderTemplate(w, r, "make-reservation.page.html", &Models.TemplateData{
+	_ = render.RenderTemplate(w, r, "make-reservation.page.html", &Models.TemplateData{
 		Data: data,
 		Form: forms.New(nil),
 	})
@@ -93,7 +93,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		data["reservation"] = reservation // store the reservation-data and pass it to template
 
 		// re-render this page to show some error
-		render.RenderTemplate(w, r, "make-reservation.page.html", &Models.TemplateData{
+		_ = render.RenderTemplate(w, r, "make-reservation.page.html", &Models.TemplateData{
 			Form: form,
 			Data: data,
 		})
@@ -111,17 +111,17 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 // Generals renders the room page
 func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "generals.page.html", &Models.TemplateData{})
+	_ = render.RenderTemplate(w, r, "generals.page.html", &Models.TemplateData{})
 }
 
 // Majors renders the room page
 func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "majors.page.html", &Models.TemplateData{})
+	_ = render.RenderTemplate(w, r, "majors.page.html", &Models.TemplateData{})
 }
 
 // Availability renders the Book Now page
 func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "search-availability.page.html", &Models.TemplateData{})
+	_ = render.RenderTemplate(w, r, "search-availability.page.html", &Models.TemplateData{})
 }
 
 // PostAvailability handle the post from form in search-availability page
@@ -129,7 +129,7 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	start := r.Form.Get("start")
 	end := r.Form.Get("end")
 
-	w.Write([]byte(fmt.Sprintf("The start date is %s and The end date is %s", start, end)))
+	_, _ = w.Write([]byte(fmt.Sprintf("The start date is %s and The end date is %s", start, end)))
 }
 
 type jsonResponse struct {
@@ -150,12 +150,12 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(out)
+	_, _ = w.Write(out)
 }
 
 // Contact renders the contact page
 func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "contact.page.html", &Models.TemplateData{})
+	_ = render.RenderTemplate(w, r, "contact.page.html", &Models.TemplateData{})
 }
 
 // ReservationSummary renders the reservation-summary page
@@ -174,7 +174,7 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 	data := make(map[string]interface{})
 	data["reservation"] = reservation
 
-	render.RenderTemplate(w, r, "reservation-summary.page.html", &Models.TemplateData{
+	_ = render.RenderTemplate(w, r, "reservation-summary.page.html", &Models.TemplateData{
 		Data: data,
 	})
 }
