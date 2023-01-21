b8a5306a0da42e2e2c9e43aa24202dd2ef1a7b05
454270186 <sc21ey@leeds.ac.uk>
2023-01-21 11:03:18 +0800

    add migration for seeding rooms and restrictions

---
+++ migrations/20230121024722_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name, created_at, updated_at) VALUES
+('General''s Quarters','2023-01-18 00:00:00.000000','2023-01-18 00:00:00.000000'),
+('Major''s Suite','2023-01-18 00:00:00.000000','2023-01-18 00:00:00.000000');
---
+++ migrations/20230121025439_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name, created_at, updated_at) VALUES
+('Reservation','2023-01-18 00:00:00.000000','2023-01-18 00:00:00.000000'),
+('Owner Block','2023-01-19 00:00:00.000000','2023-01-19 00:00:00.000000');
