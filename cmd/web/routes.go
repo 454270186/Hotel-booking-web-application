fac9abe9afd26906d6ace0f4f88cb48fe84a2bfd
454270186 <sc21ey@leeds.ac.uk>
2023-01-24 14:18:10 +0800

    add go-template builtin FuncMap

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -53,7 +53,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	mux.Route("/admin", func(mux chi.Router) {
-		mux.Use(Auth)
+		//mux.Use(Auth)
 
 		mux.Get("/dashboard", handler.Repo.AdminDashboard)
 		mux.Get("/reservations-new", handler.Repo.AdminNewReservations)
--- internal/render/render.go
+++ internal/render/render.go
@@ -10,11 +10,21 @@ import (
 	"html/template"
 	"net/http"
 	"path/filepath"
+	"time"
 )
 
+var functions = template.FuncMap{
+	"humanDate": HumanDate,
+}
 var app *config.AppConfig
 var pathToTemplates = "./templates"
 
+// HumanDate returns time in YYYY-MM-DD format
+func HumanDate(t time.Time) string {
+	return t.Format("2006-01-02")
+}
+
+// NewRenderer sets the config for the template package
 func NewRenderer(a *config.AppConfig) {
 	app = a
 }
@@ -81,7 +91,7 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 	// range through all files ending with *.page.html
 	for _, page := range pages {
 		name := filepath.Base(page) // get name like "*.page.html"
-		ts, err := template.New(name).ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return myCache, err
 		}
