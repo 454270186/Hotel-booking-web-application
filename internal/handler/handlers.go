c3e722f762114c783e84c37dbb7f584e19b28a46
454270186 <sc21ey@leeds.ac.uk>
2023-01-28 00:53:44 +0800

    add reservation calendar checkbox func

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -633,7 +633,7 @@ func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Re
 				}
 			} else {
 				// it's a block
-				blockMap[y.StartDate.Format("2006-01-2")] = y.RestrictionID
+				blockMap[y.StartDate.Format("2006-01-2")] = y.ID
 			}
 		}
 
