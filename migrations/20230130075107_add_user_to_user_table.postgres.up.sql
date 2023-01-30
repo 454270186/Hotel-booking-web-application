a551cddd75f0bfaa5b2d601422920d807b636254
454270186 <sc21ey@leeds.ac.uk>
2023-01-30 16:42:49 +0800

    add migration

---
+++ migrations/20230130075107_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."users"("first_name","last_name","email","password","access_level","created_at","updated_at")
+VALUES
+(E'Erfei',E'Yu',E'me@me.com',E'$2a$12$o9gGQbVFE3WpRuZLgmWpgeHSbHzPabZ4vRp3H4m0RYtwdFJMtdku.',3,E'2023-01-23 00:00:00.000000',E'2023-01-23 00:00:00.000000')
